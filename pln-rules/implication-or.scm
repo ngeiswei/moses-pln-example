0d8dc2251feb4ec82c5a047d66cb826b8fb7e856
Nil Geisweiller <ngeiswei@gmail.com>
2015-07-22 01:43:36 +0300

    Fix formula of implication-or rule

--- pln-rules/implication-or.scm
+++ pln-rules/implication-or.scm
@@ -63,21 +63,45 @@
               (VariableNode "$B")
               (VariableNode "$C")
            )
+           (VariableNode "$A")
+           (VariableNode "$B")
+           (VariableNode "$C")
         )
      )
   )
 )
 
-(define (pln-formula-implication-or ABC AC BC)
+(define (pln-formula-implication-or ABC AC BC A B C)
   (cog-set-tv! ABC
-   (pln-formula-implication-or-side-effect-free AC BC))
+   (pln-formula-implication-or-side-effect-free AC BC A B C))
 )
 
-;; WARNING: same as Or formula, not sure that is correct
-(define (pln-formula-implication-or-side-effect-free AC BC)
-  (let 
+;; Computing the strength is based on
+;;
+;; P(C|A) = P(C,A)/P(A)
+;; P(C|B) = P(C,B)/P(B)
+;;
+;; P(A U B) = P(A)+P(B)-P(A)*P(B)
+;;
+;; P(C|A U B) = P(C,A U B)/P(A U B)
+;; = (P(C,A)+P(C,B)-P(C,A,B))/P(A U B)
+;;
+;; Let's assume that P(C,A,B) = P(C,A)*P(C,B), then we can write
+;;
+;; P(C|A U B) = (P(C|A)*P(A) + P(C|B)*P(B) - P(C|A)*P(A)*P(C|B)*P(B)
+;;            / (P(A) + P(B) - P(A)*P(B))
+;;
+(define (pln-formula-implication-or-side-effect-free AC BC A B C)
+  (let* 
       ((sAC (cog-stv-strength AC))
        (sBC (cog-stv-strength BC))
+       (sA (cog-stv-strength A))
+       (sB (cog-stv-strength B))
+       (sC (cog-stv-strength C))
        (cAC (cog-stv-confidence AC))
-       (cBC (cog-stv-confidence BC)))
-    (stv (- (+ sAC sBC) (* sAC sBC)) (min cAC cBC))))
+       (cBC (cog-stv-confidence BC))
+       (CinterA (* sAC sA))
+       (CinterB (* sBC sB)))
+    (stv (/ (+ CinterA CinterB (* CinterA CinterB sA sB -1))
+            (+ sA sB (* sA sB -1)))
+         (min cAC cBC))))
